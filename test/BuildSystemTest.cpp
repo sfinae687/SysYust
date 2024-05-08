f2eb1791425ffa06d82fd355fc49d6f1e67471a9
ll06printf <ll06nest@outlook.com>
2024-05-08 12:37:11 +0800

    <feature> 构建系统：添加argparse库
    - 引入argparse库
    - 添加对argparse库的基本测试
    On branch buildsystem
    	modified:   CMakeLists.txt
    	modified:   test/BuildSystemTest.cpp
    	modified:   test/CMakeLists.txt

--- test/BuildSystemTest.cpp
+++ test/BuildSystemTest.cpp
@@ -2,6 +2,7 @@
 
 #include <gtest/gtest.h>
 #include <fmt/core.h>
+#include <argparse/argparse.hpp>
 
 TEST(BuildSystemTest, GTestTest) {
     ASSERT_TRUE(true) << "Google Test has been installed correctly";
@@ -9,4 +10,11 @@ TEST(BuildSystemTest, GTestTest) {
 
 TEST(BuildSystemTest, fmtTest) {
     ASSERT_EQ("FormatInt:0", fmt::format("FormatInt:{}", 0));
+}
+
+TEST(BuildSystemTest, argparseTest) {
+    argparse::ArgumentParser program("ParserTest");
+    program.add_argument("aInt").scan<'i',int>();
+    program.parse_args({"./main", "0"});
+    ASSERT_EQ(program.get<int>("aInt"), 0);
 }
