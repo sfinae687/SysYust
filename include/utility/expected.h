30a9a01ceec2b45b2487a0ea5a253c03928b812c
ll06printf <ll06nest@outlook.com>
2024-05-16 17:29:55 +0800

    <feature> utility：引入expected实现
    - 引用 zeus::expected 并导入到 SysYust命名空间
    On branch f_expected
    	modified:   .gitmodules
    	modified:   CMakeLists.txt
    	modified:   include/utility/expected.h
    	new file:   lib/expected
    	modified:   src/utility/CMakeLists.txt

--- include/utility/expected.h
+++ include/utility/expected.h
@@ -3,11 +3,18 @@
 #ifndef SYSYUST_UTILITY_EXPECTED_H
 #define SYSYUST_UTILITY_EXPECTED_H
 
+#include <zeus/expected.hpp>
+
 namespace SysYust {
 
-    class expected {
+    using zeus::unexpect_t;
+    using zeus::unexpect;
 
-    };
+    template<typename E>
+    using unexpected = zeus::unexpected<E>;
+
+    template<typename T, typename E>
+    using expected = zeus::expected<T, E>;
 
 } // SysYust
 
