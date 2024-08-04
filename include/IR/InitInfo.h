023e4a231821ab7c6b0b84dc24fa6abf4efd9059
ll06printf <ll06nest@outlook.com>
2024-08-04 09:53:20 +0800

    为InitInfo添加了一些检测函数和新的undef值表示方式。

--- include/IR/InitInfo.h
+++ include/IR/InitInfo.h
@@ -15,7 +15,7 @@ namespace SysYust::IR {
         using value_type = im_symbol;
         using var_type = var_symbol;
         using list_type = std::vector<InitInfo*>;
-        using data_type = std::variant<value_type, var_type, list_type>;
+        using data_type = std::variant<value_type, var_type, list_type, std::nullptr_t>;
 
         InitInfo() = default;
         /*NOLINT*/ InitInfo(var_symbol var);
@@ -31,6 +31,15 @@ namespace SysYust::IR {
         [[nodiscard]] bool is_list() const {
             return _data.index() == 1;
         }
+        [[nodiscard]] bool is_value() const {
+            return _data.index() == 0;
+        }
+        [[nodiscard]] bool is_var() const {
+            return _data.index() == 1;
+        }
+        [[nodiscard]] bool is_undef() const {
+            return _data.index() == 3 || is_value() && value() == im_symbol(im_symbol::undef);
+        }
         [[nodiscard]] value_type value() const {
             return std::get<value_type>(_data);
         }
@@ -41,11 +50,10 @@ namespace SysYust::IR {
             return std::get<var_type>(_data);
         }
 
-
     private:
         void release();
         static list_type copy_list(const list_type &li);
-        data_type _data = im_symbol(im_symbol::undef);
+        data_type _data = nullptr;
     };
 
 } // IR
